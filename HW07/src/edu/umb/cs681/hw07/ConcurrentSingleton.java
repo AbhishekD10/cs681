ec4fbbc345aa86045c2c3c8f3a3a33d8ed636c98
Abhishek Doshi <53604173+AbhishekD10@users.noreply.github.com>
2022-03-20 21:30:21 -0400

    HomeWork 7 Uploaded

--- HW07/src/edu/umb/cs681/hw07/ConcurrentSingleton.java
+++ HW07/src/edu/umb/cs681/hw07/ConcurrentSingleton.java
@@ -2,24 +2,33 @@ package edu.umb.cs681.hw07;
 
 import java.util.concurrent.locks.ReentrantLock;
 
-public class ConcurrentSingleton {
-	
-	private static ConcurrentSingleton instance = null;
-	private static ReentrantLock lock = new ReentrantLock();
-	
-	private ConcurrentSingleton() {
-	}
+public class ConcurrentSingleton{
+    private ConcurrentSingleton() {
+    };
 
-	public static ConcurrentSingleton getInstance() {
-		
-		try {
-			lock.lock();
-			if (instance == null)
-				instance = new ConcurrentSingleton();
-			return instance;
-		} finally {
-			lock.unlock();
-		}
-	}
+    private static ConcurrentSingleton instance = null;
+    private static ReentrantLock lock = new ReentrantLock();
+
+
+    public static ConcurrentSingleton getInstance() {
+        lock.lock();
+        try {
+            if (instance == null) {
+                instance = new ConcurrentSingleton();
+            }
+            return instance;
+        } finally {
+            lock.unlock();
+        }
+    }
+    public static void main(String[] args)  {
+
+        for (int i = 0; i < 15; i++) {
+            Thread t = new Thread(() -> {
+                System.out.println(ConcurrentSingleton.getInstance());
+            });
+            t.start();
+        }
+    }
 
 }
