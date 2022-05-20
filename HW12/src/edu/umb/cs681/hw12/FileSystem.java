ad7d24ff9484d09a04737a19eac9f99008627131
Abhishek Doshi <53604173+AbhishekD10@users.noreply.github.com>
2022-05-20 14:44:17 -0400

    Homework 12 added

--- HW12/src/edu/umb/cs681/hw12/FileSystem.java
+++ HW12/src/edu/umb/cs681/hw12/FileSystem.java
@@ -31,12 +31,18 @@ public class FileSystem implements Runnable {
 
     public static void main(String[] args) {
 
-        LocalDateTime localTime = LocalDateTime.now();
+    	LocalDateTime localTime = LocalDateTime.now();
         Directory root = new Directory(null, "root", 0, localTime);
         Directory applications = new Directory(root, "applications", 0, localTime);
+        Directory home = new Directory(root, "home", 0, localTime);
+        Directory code = new Directory(home, "code", 0, localTime);
         FileSystem fs = new FileSystem();
         File a = new File(applications, "a", 200, localTime);
         File b = new File(applications, "b", 650, localTime);
+        File c = new File(home, "c", 800, localTime);
+        File d = new File(home, "d", 200, localTime);
+        File e = new File(code, "e", 600, localTime);
+        File f = new File(code, "f", 320, localTime);
         Thread t1 = new Thread(fs);
         t1.start();
         try {
@@ -44,14 +50,11 @@ public class FileSystem implements Runnable {
         } catch (InterruptedException e1) {
             e1.printStackTrace();
         }
-        System.out.println("Directory "+root.getName()+" is created.");
+        System.out.println("Directory "+root.getName()+" is created");
         System.out.println("File "+a.getName()+" added to the directory "+root.getName());
         System.out.println("File "+b.getName()+" added to the directory "+root.getName());
 
 
-        Directory home = new Directory(root, "home", 0, localTime);
-        File c = new File(home, "c", 800, localTime);
-        File d = new File(home, "d", 200, localTime);
         Thread t2 = new Thread(fs);
         t2.start();
         try {
@@ -59,13 +62,10 @@ public class FileSystem implements Runnable {
         } catch (InterruptedException e2) {
             e2.printStackTrace();
         }
-        System.out.println("Directory "+home.getName()+" is created.");
-        System.out.println("File "+c.getName()+" added to the directory "+home.getName()+".");
-        System.out.println("File "+d.getName()+" added to the directory "+home.getName()+".");
+        System.out.println("Directory "+home.getName()+" is created");
+        System.out.println("File "+c.getName()+" added to the directory "+home.getName());
+        System.out.println("File "+d.getName()+" added to the directory "+home.getName());
 
-        Directory code = new Directory(home, "code", 0, localTime);
-        File e = new File(code, "e", 600, localTime);
-        File f = new File(code, "f", 320, localTime);
         Thread t3 = new Thread(fs);
         t3.start();
         try {
@@ -73,9 +73,9 @@ public class FileSystem implements Runnable {
         } catch (InterruptedException e3) {
             e3.printStackTrace();
         }
-        System.out.println("Directory "+code.getName()+" is created.");
-        System.out.println("File "+e.getName()+" added to the directory "+code.getName()+".");
-        System.out.println("File "+f.getName()+" added to the directory "+code.getName()+".");
+        System.out.println("Directory "+code.getName()+" is created");
+        System.out.println("File "+e.getName()+" added to the directory "+code.getName());
+        System.out.println("File "+f.getName()+" added to the directory "+code.getName());
 
     }
 
